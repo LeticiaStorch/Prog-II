25bb0dfc3598d411cb571dea8e49fd5a600bf415
Leticia Storch <storch29@gmail.com>
2025-05-08 22:09:57 -0300

    Adicionando

--- Revisão/rev_03/rev_03.c
+++ Revisão/rev_03/rev_03.c
@@ -3,4 +3,18 @@
 int main()
 {
     int qnt;
+    scanf("%d", &qnt);
+
+    int livros[qnt];
+    
+    for(int i = 0; i < qnt; i++)
+    {
+        scanf("%d ", &livros[i]);
+    }
+    
+
+   /* for(int i = 0; i < qnt; i++)
+    {
+        printf("%d ", livros[i]);    
+    }*/
 }
